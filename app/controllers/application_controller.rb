c97103810cd2af5ad6ff9d2fb5381b7597ca68f5
Zeljko Kalezic <zeljkokalezic@gmail.com>
2015-08-08 06:05:08 +0000

    Hello world view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   def hello
-    render text: "hello, world!"
+    #render text: "hello, world!"
+    @time = Time.new
   end
   
 end
